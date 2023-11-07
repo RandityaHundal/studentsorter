197719eb7d971d3c01554ec827effac06a575a2c
Randitya Hundal <67023582+RandityaHundal@users.noreply.github.com>
2023-11-06 19:55:08 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("How many students are in your class?: ");
+int studentnumber = Convert.ToInt32(Console.ReadLine());
+
+string[] students = new string[studentnumber];
+
+Console.WriteLine("\nInput the name of the students");
+
+for (int i = 0; i < studentnumber; i++)
+{
+    students[i] = Console.ReadLine();
+}
+
+Array.Sort(students);
+
+Console.WriteLine("Here they are alphabetically sorted");
+
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine(students[i]);
+}
+
+// close key
+Console.ReadKey();
